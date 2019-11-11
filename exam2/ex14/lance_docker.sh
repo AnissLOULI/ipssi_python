4f5bb922117c3b3ed0dd81186895544cd27f2fb5
AnissLOULI <aniss.louli@hotmail.fr>
2019-11-11 15:57:22 +0100

    MAJ ex14-15

---
+++ exam2/ex14/lance_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -v $(out):/tmp ipssi/ex14:1 .
---
+++ exam2/ex15/login.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+import hashlib
+
+def compare_pass(argument):
+	ipssi = 'ipssi'
+	ipssi_hash = hashlib.md5(ipssi.encode()).hexdigest()
+	print("md5 \"ipssi\" : ", ipssi_hash)
+
+	pass_hash = hashlib.md5(argument.encode()).hexdigest()
+	print("md5    pass : ", pass_hash)
+
+	if ipssi_hash == pass_hash:
+		return True
+	else:
+		return False

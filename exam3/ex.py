0fa39a4e66fefe1741903350dd47e6a935ab9680
AnissLOULI <aniss.louli@hotmail.fr>
2019-12-07 10:27:38 +0100

    x

--- exam3/ex.py
+++
@@ -1,8 +0,0 @@
-import sys
-import calendar
-from datetime import datetime
-from tree import show_tree
-
-def show_noel(date_donnee):
-    date_donnee = datetime.strptime(str(date_donnee), '%Y-%m-%d')
-    return date_donnee

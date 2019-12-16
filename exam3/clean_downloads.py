a85c991d2700ddd41b650d1b6806d95347a56d96
AnissLOULI <aniss.louli@hotmail.fr>
2019-12-16 19:26:09 +0100

    xx

--- exam3/clean_downloads.py
+++ exam3/clean_downloads.py
@@ -12,30 +12,33 @@ def clean_downloads (dossier):
     liste_fichier = os.listdir (dossier)
     
     if len (liste_fichier) == 0:
-        print ('Aucun fichier dans ce dossier !')
+        print ('No file in directory !')
     else:
         print ("cleanup old files : (more than 10 days + 50MB)\n")
         for fichier in liste_fichier:
-            jours_depuis_creation = (datetime.now() - datetime.fromtimestamp(os.stat(dossier+fichier).st_atime)).days
+            jours_depuis_creation = (datetime.now() - datetime.fromtimestamp(os.stat(dossier+fichier).st_mtime)).days
             taille_fichier = os.stat (dossier+fichier).st_size
-            if jours_depuis_creation > 10 or taille_fichier > 6250000:
+            if jours_depuis_creation > 10 and taille_fichier > 6250000:
                 print (fichier)
                 choix = input ("[yes/No]?")
-                if choix == "yes":
+                if choix == "y":
                     os.remove (dossier+fichier)
                     print (fichier+" delete\n")
                 else:
                     print (fichier+" keep\n")
             else:
-                print (fichier)
-                print ("organizing your files :")
-                a = str(datetime.fromtimestamp(os.stat(dossier+fichier).st_atime).year)+"-"+str(datetime.fromtimestamp(os.stat(dossier+fichier).st_atime).month)
-                try:
-                    os.mkdir (dossier+a)
-                except:
-                    print ("dossier déja exitant")
-                print ("moving into "+a+" : "+fichier+"\n") 
-                shutil.move (dossier+fichier,dossier+a)
+                if jours_depuis_creation > 10:
+                    print (fichier)
+                    print ("organizing your files :")
+                    annee_mois_du_fichier = str(datetime.fromtimestamp(os.stat(dossier+fichier).st_atime).year)+"-"+str(datetime.fromtimestamp(os.stat(dossier+fichier).st_atime).month)
+                    try:
+                        os.mkdir (dossier+annee_mois_du_fichier)
+                    except:
+                        print ("directory exists already")
+                    print ("moving into "+annee_mois_du_fichier+" : "+fichier+"\n") 
+                    shutil.move (dossier+fichier,dossier+annee_mois_du_fichier)
+                
+
 
                     
                 
